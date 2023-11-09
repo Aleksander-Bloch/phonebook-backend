b94580d38879a0aa0eeb5ec7f5d2906cd42c96fe
Aleksander Bloch <alekbloch21@gmail.com>
2023-11-09 15:20:00 +0100

    Finish phonebook backen step8 - morgan request body

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+morgan.token(
+  'body',
+  request => request.method === 'POST' && JSON.stringify(request.body)
+)
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
 
 app.get('/api/persons', (request, response) => {
   response.json(persons)
